4ab44948becdfdf4f002176405c686d5da07590f
Sean Smith <sean.smithy1@gmail.com>
2013-04-19 20:13:20 +1000

    130419 ch9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,6 +25,7 @@ describe "Authentication" do
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
+  end
 
   describe "with valid information" do
     let(:user) { FactoryGirl.create(:user) }
@@ -44,6 +45,26 @@ describe "Authentication" do
   end
 
 describe "authorization" do
+
+ describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+        end
+      end
+
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
