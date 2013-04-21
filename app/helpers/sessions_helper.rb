5c94a7fa75bc00f7c5cbc18dc832152ca9542cc1
Sean Smith <sean.smithy1@gmail.com>
2013-04-21 14:39:06 +1000

    130421-User Maint

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,8 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by_remember_token(cookies[:remember_token])  end
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
  
   def current_user?(user)
     user == current_user
